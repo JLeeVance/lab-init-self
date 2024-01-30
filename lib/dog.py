f50d207cd638390796545ee600fa28089827a759
J Vance <justinleevance94@gmail.com>
2024-01-30 15:10:01 -0500

    intro lab done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+    
+gracie = Dog("Gracie", "Labradoodle")
+print(gracie.name)
